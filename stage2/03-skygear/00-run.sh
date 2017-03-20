8176ca4636540ac9b8b4fc19e169b325118cf67f
akiroz <akiroz.vectis@gmail.com>
2017-03-20 17:36:27 +0800

    use nodejs built for ARMv6

--- stage2/03-skygear/00-run.sh
+++ stage2/03-skygear/00-run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -e
 
+VERSION=v7.7.1
+
 on_chroot << EOF
-curl -sL https://deb.nodesource.com/setup_7.x | bash -
+mkdir temp
+cd temp
+wget https://nodejs.org/dist/$VERSION/node-$VERSION-linux-armv6l.tar.gz
+tar -xzf node-$VERSION-linux-armv6l.tar.gz
+mv node-$VERSION-linux-armv6l /opt/nodejs/
+ln -s /opt/nodejs/bin/node /usr/bin/node
+ln -s /opt/nodejs/bin/node /usr/sbin/node
+ln -s /opt/nodejs/bin/node /sbin/node
+ln -s /opt/nodejs/bin/node /usr/local/bin/node
+ln -s /opt/nodejs/bin/npm /usr/bin/npm
+ln -s /opt/nodejs/bin/npm /usr/sbin/npm
+ln -s /opt/nodejs/bin/npm /sbin/npm
+ln -s /opt/nodejs/bin/npm /usr/local/bin/npm
 EOF
