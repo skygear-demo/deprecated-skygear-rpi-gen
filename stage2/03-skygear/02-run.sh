00f55cf9eeb3d9357022618f2ec2d4bc957ba045
akiroz <akiroz.vectis@gmail.com>
2017-04-20 15:39:31 +0800

    use skygear-rpi-launcher repo

--- stage2/03-skygear/02-run.sh
+++ stage2/03-skygear/02-run.sh
@@ -2,10 +2,10 @@
 
 on_chroot << EOF
 cd /home/pi
-su -c 'git clone https://github.com/akiroz/skygear-iot.git' pi
-cd skygear-iot
+su -c 'git clone https://github.com/skygear-demo/skygear-rpi-launcher.git' pi
+cd skygear-rpi-launcher
 su -c 'npm install' pi
-cp skygear-iot.service /etc/systemd/system/
+cp skygear-rpi-launcher.service /etc/systemd/system/
 systemctl daemon-reload
-systemctl enable skygear-iot
+systemctl enable skygear-rpi-launcher
 EOF
